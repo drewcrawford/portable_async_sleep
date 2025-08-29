a0dd77441816019f27361fb406abe6a9702c8ce8
Drew Crawford <drew@sealedabstract.com>
2025-08-28 23:40:39 -0500

    Add stdlib headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-License-Identifier: MIT OR Apache-2.0
 /*!
 
 ![logo](../../../art/logo.png)
--- src/wasm.rs
+++ src/wasm.rs
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
 //SPDX-License-Identifier: MIT OR Apache-2.0
 /*!
 WASM implementation using browser's setTimeout API.
